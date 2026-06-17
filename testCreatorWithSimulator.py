0c9220495803e90b8bd2e4fe73d9bccc3545c2ec
MagnusBane05 <evman@rogers.com>
2026-06-17 07:08:32 -0400

    switched to IntEnum

--- testCreatorWithSimulator.py
+++ testCreatorWithSimulator.py
@@ -10,11 +10,11 @@ class TestCreatorWithSimulator(unittest.TestCase):
     def test_5_player(self):
         self._test_x_player(self, 5, 1000)
 
-    # def test_10_player(self):
-    #     self._test_x_player(self, 10, 1000)
+    def test_10_player(self):
+        self._test_x_player(self, 10, 10)
 
-    def test_15_player(self):
-        self._test_x_player(self, 15, 10)
+    # def test_15_player(self):
+    #     self._test_x_player(self, 15, 10)
 
     @staticmethod
     def _test_x_player(clazz: TestCreatorWithSimulator, x: int, n: int):
@@ -49,6 +49,19 @@ class TestCreatorWithSimulator(unittest.TestCase):
         print(f"Total: {sum(times)}")
         print()
         
+def profile_x_player(x: int, n: int):
+    game: Game = {
+        'players': x
+    }
+    seed = 193
+    result = simulate_info(n, game, seed)
+    for (info_list, death_info), true_world in result:
+        grim_manager = GrimoireManager(game, true_world)
+
+        nights = len(true_world.get_unique_nights())
+        grim_manager.add_full_game(info_list, death_info, nights)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    profile_x_player(10, 10)
